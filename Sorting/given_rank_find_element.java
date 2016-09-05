4910e216da4a3265db54b9cf8d4e39cd3bd887d3
brad <lokenders801@gmail.com>
2016-09-05 20:07:31 +0530

    Create given_rank_find_element.java

---
+++ Sorting/given_rank_find_element.java
@@ -0,0 +1,26 @@
+                                                                                           /* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int rank=sc.nextInt();
+		int []arr=new int[n];
+		
+		for(int i=0;i<n;i++)
+		   arr[i]=sc.nextInt();
+		   
+		Arrays.sort(arr);
+	//rank = total elem -rank => 10-3 =7=> .....8 9 10 => index of 8 is 7 => arr[total-rank]
+	
+	System.out.println(arr[n-rank]);
+	}
+	
+}
