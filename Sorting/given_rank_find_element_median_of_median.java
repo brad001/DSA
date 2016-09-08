c0b985804fb13ef3a966b8e9dfbea94084336374
brad <lokenders801@gmail.com>
2016-09-08 13:14:37 +0530

    Update given_rank_find_element_median_of_median.java

--- Sorting/given_rank_find_element_median_of_median.java
+++ Sorting/given_rank_find_element_median_of_median.java
@@ -7,17 +7,93 @@ import java.io.*;
 /* Name of the class has to be "Main" only if the class is public. */
 class Ideone
 {
-	int get_pivot_index(int low,int high)
+	int median_of_median(int[]arr,int low,int high)
 	{
-		int index=(int)(Math.random()*(high-low+1)+low);
-	     System.out.println("random index chose is: "+index);
-		return index;
+		
+	if(high-low+1>=5)
+	{
+	  int n=high-low+1;
+	  int num_groups=n/5;
+	  int rem_elem=n%5;
+	  
+	  int start_index=low,end_index=low+4;
+	  int index_median=low;
+	  for(int i=0;i<num_groups;i++)
+	  {
+	  	
+	  	Arrays.sort(arr,start_index,end_index+1);
+	  	System.out.println("median is : "+arr[(start_index+end_index)/2]);
+	  	int t1=arr[(start_index+end_index)/2];
+	  	arr[(start_index+end_index)/2]=arr[index_median];
+	  	arr[index_median]=t1;
+	  	
+	  	index_median++;
+	  	//arr[index_median++]=arr[(start_index+end_index)/2];
+	  	start_index=end_index+1;
+	  	end_index=end_index+5;
+	  }
+	   
+	   //for the rest
+	   
+	   int low_index=num_groups*5+low;
+	   Arrays.sort(arr,low_index,high+1);
+	   
+	   if(rem_elem>0)
+	   {
+	    if(rem_elem%2==1)//odd
+	    {
+	   	System.out.println("median is : "+arr[low_index+(rem_elem/2)]);
+	   	int t1=arr[low_index+(rem_elem/2)];
+	   	arr[low_index+(rem_elem/2)]=arr[index_median];
+	   	arr[index_median]=t1;
+	   //arr[index_median++]=arr[low_index+(rem_elem/2)];
+	    }
+	    else//even
+	    {
+	   	System.out.println("median is : "+arr[low_index+(rem_elem/2-1)]);
+	   	int t1=arr[low_index+(rem_elem/2-1)];
+	   	arr[low_index+(rem_elem/2-1)]=arr[index_median];
+	   	arr[index_median]=t1;
+	   //arr[index_median++]=arr[low_index+(rem_elem/2-1)];
+	    }
+	  }
+	   
+	  return median_of_median(arr,low,index_median-1); 
+	   
+	}
+	else
+	{
+		int size=high-low+1;
+		if(size%2==0)//even
+		  //return arr[low+(size/2)-1];
+		  //return index
+		  return low+(size/2)-1;
+		else //odd  
+	    //return arr[low+size/2];
+	    return low+size/2;
+	}
+	
 	}
 	int partition(int[]arr,int low,int high)
 	{
-		int pivot_index=
-		int pivot=
-		System.out.println("pivot chosen is: "+pivot);
+		//first element chosen as pivot
+	    //int ret=median_of_median(arr,low,high);
+	   // int t1=arr[low];
+	  //  arr[low]=ret;
+	   // arr[]
+	    int p_index=median_of_median(arr,low,high);
+	    System.out.println("index is "+p_index+"pivot is "+arr[p_index]);
+	    
+	    System.out.println("before partiotion");
+	    			for(int elem:arr)
+			  System.out.print(elem+" ");
+			  System.out.println();
+			  
+	    
+		int pivot=arr[p_index];
+		int t1=pivot;
+		arr[p_index]=arr[low];
+		arr[low]=t1;
 		
 		int i=low+1;
 		int j=high;
@@ -44,8 +120,10 @@ class Ideone
 		}
 		int temp=arr[j];
 		arr[j]=pivot;
-		arr[pivot_index]=temp;
+		arr[low]=temp;
 		
+		
+		//System.out.println("after partiotion: ");
 		
 		return j;
 	}
@@ -80,88 +158,26 @@ class Ideone
 		 return arr[low];
 		
 	}
-	int median_of_median(int[]arr,int low,int high)
-	{
-		
-	if(high-low+1>=5)
-	{
-	  int n=high-low+1;
-	  int num_groups=n/5;
-	  int rem_elem=n%5;
-	  
-	  int start_index=0,end_index=4;
-	  int index_median=0;
-	  for(int i=0;i<num_groups;i++)
-	  {
-	  	
-	  	Arrays.sort(arr,start_index,end_index+1);
-	  	System.out.println("median is : "+arr[(start_index+end_index)/2]);
-	  	arr[index_median++]=arr[(start_index+end_index)/2];
-	  	start_index=end_index+1;
-	  	end_index=end_index+5;
-	  }
-	   
-	   //for the rest
-	   
-	   int low_index=num_groups*5;
-	   Arrays.sort(arr,low_index,n);
-	   
-	   if(rem_elem%2==1)//odd
-	   {
-	   	System.out.println("median is : "+arr[low_index+(rem_elem/2)]);
-	   arr[index_median++]=arr[low_index+(rem_elem/2)];
-	   }
-	   else//even
-	   {
-	   	System.out.println("median is : "+arr[low_index+(rem_elem/2-1)]);
-	   arr[index_median++]=arr[low_index+(rem_elem/2-1)];
-	   }
-	   
-	  return median_of_median(arr,0,index_median-1); 
-	   
-	}
-	else
-	{
-		int size=high-low+1;
-		if(size%2==0)//even
-		  return arr[low+(size/2)-1];
-		else //odd  
-	    return arr[low+size/2];
-	}
-	
-	}
 	public static void main (String[] args) throws java.lang.Exception
 	{
 	
 	   Scanner sc=new Scanner(System.in);
 	   int n=sc.nextInt();
-	   
 	   int []arr=new int[n];
-	   
+	   int rank=sc.nextInt();
 	   for(int i=0;i<n;i++)
 	     arr[i]=sc.nextInt();
+	   Ideone obj=new Ideone();
 	   
-	   
-	   System.out.println("given array");
+	   System.out.println("before sorting");
 	   for(int elem:arr)
 	      System.out.print(elem+" ");
-	    System.out.println();
-	  
-	  int[]temp_arr=new int[n];
-	  for(int i=0;i<n;i++)
-	  temp_arr[i]=arr[i];
-	  Arrays.sort(temp_arr);
-	  
-	   System.out.println("sorted array");
-	   for(int elem:temp_arr)
+	   
+	   int returned_elem=obj.q_sort(arr,0,n-1,rank);
+	   System.out.println("the element with given rank is : "+returned_elem);
+	   
+	   System.out.println("after sorting");
+	   for(int elem:arr)
 	      System.out.print(elem+" ");
-	    System.out.println();
-	  
-	  //get the median
-	  Ideone obj=new Ideone();
-	  System.out.println(obj.median_of_median(arr,0,n-1));
-       
-	      	    
-	      	    
 	}
 }
